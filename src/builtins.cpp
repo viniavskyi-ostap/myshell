a59b3aca8a62a2bbfc2bc2d3cec2c12bf4929da7
ostapViniavskyi <viniavskyi@ucu.edu.ua>
2019-10-22 11:33:57 +0300

    Add mexit and merrno implementation

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -3,10 +3,38 @@
 //
 
 #include <iostream>
+#include <cstring>
+#include <string>
 #include "builtins.h"
 
+static int get_argc(char **argv){
+    int i = 0;
+    for(; argv[i]; ++i);
+    return i;
+}
+
 int mexit(char **argv) {
-    exit(0);
+    int argc = get_argc(argv);
+
+    int exit_code = 0;
+    if (argc > 1){
+        for (int i = 1; i < argc; ++i){
+            if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
+                std::cout << "Exit with given exit code.\n"
+                             "Usage: mexit [exit_code] [-h|--help]" << std::endl;
+                return 0;
+            }
+        }
+        try {
+            std::cout << "here" << std::endl;
+            exit_code = std::stoi(argv[1]);
+        }
+        catch (std::invalid_argument &e) {
+            std::cerr << "mexit: Illegal number: " << argv[1] << std::endl;
+            return 1;
+        }
+    }
+    exit(exit_code);
 }
 
 int mpwd(char **argv, const std::string &current_path) {
@@ -30,6 +58,16 @@ int mexport(char **argv, environment_variables &env) {
 }
 
 int merrno(char **argv, int error_code) {
-    std::cout << "MERRNO: " << error_code << "\n";
+    int argc = get_argc(argv);
+    if (argc > 1) {
+        for (int i = 1; i < argc; ++i) {
+            if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
+                std::cout << "Print return code of the last command.\n"
+                             "Usage: merrno [-h|--help]" << std::endl;
+                return 0;
+            }
+        }
+    }
+    std::cout << error_code << std::endl;
     return 0;
 }
