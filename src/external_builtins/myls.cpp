8a0ddb02fad5b6b7c86929c22d7569d5897d86f0
andriiprysiazhnyk <prysiazhnyk@ucu.edu.ua>
2019-10-29 17:33:08 +0200

    Refactor

--- src/external_builtins/myls.cpp
+++ src/external_builtins/myls.cpp
@@ -20,6 +20,8 @@ public:
     std::string sort_predicate;
 };
 
+//void
+
 void parse_options(int argc, char *argv[], Options &opts) {
     try {
         po::options_description desc{"Options"};
@@ -28,6 +30,8 @@ void parse_options(int argc, char *argv[], Options &opts) {
                 ("long,l", "Long format output")
                 ("sort", po::value<std::string>(), "Sort predicate output")
                 ("reverse,r", "Reverse output order")
+                ("recursive,R", "Recursive iteration")
+                ("filetype,F", "Print file types")
                 ("filename", po::value<std::vector<std::string>>()->
                         multitoken()->zero_tokens()->composing(), "filename");
 
@@ -38,18 +42,21 @@ void parse_options(int argc, char *argv[], Options &opts) {
         po::parsed_options parsed_options = parser.run();
         po::variables_map vm;
         po::store(parsed_options, vm);
-        if (vm.count("help")) std::cout << "myls [path|mask] [-l] [-h|--help] [--sort=U|S|t|X|D|s] [-r]\n";
+        if (vm.count("help")) {
+            std::cout << "myls [path|mask] [-l] [-h|--help] [--sort=U|S|t|X|D|s] [-r]\n";
+            exit(0);
+        }
         if (vm.count("filename"))
             std::for_each(vm["filename"].as<std::vector<std::string>>().begin(),
                           vm["filename"].as<std::vector<std::string>>().end(),
                           [&](auto &v) { opts.filenames.emplace_back(v); });
         if (vm.count("long")) opts.long_format = true;
         if (vm.count("reverse")) opts.reverse_out = true;
-        if (vm.count("sort")) opts.sort_predicate = vm["sort"].as<std::string>();
-
+        if (vm.count("sort")) opts.sort_predicate = vm["sort"].as<std::string>(); // TODO: check for correct value
     }
     catch (const po::error &ex) {
         std::cerr << ex.what() << '\n';
+        exit(1);
     }
 }
 
@@ -75,20 +82,24 @@ int main(int argc, char *argv[], char *envp[]) {
         std::cout << opts.sort_predicate << std::endl;
     }
 #endif
+
     if (opts.filenames.empty() == false) {
+        std::vector<std::string> filenames;
+
         for (auto &v: opts.filenames) {
             if (bf::exists(v)) {
-                auto status = bf::status(v);
-                std::cout << "Type is " << status.type() << std::endl;
-                std::cout << "Permissions are " << status.permissions() << std::endl;
-                std::time_t t = bf::last_write_time(v);
-                auto last_modified = std::ctime(&t);
-                std::cout << "Last write time is " << last_modified << std::endl;
-                boost::system::error_code ec;
-                boost::uintmax_t fsize = bf::file_size(v, ec);
-                if (!ec) std::cout << "File size is " << fsize << std::endl;
-                else std::cout << "File_size failed" << std::endl;
-            } else std::cout << "No file or directory found: " << v;
+//                bf::path::
+//                auto status = bf::status(v);
+//                std::cout << "Type is " << status.type() << std::endl;
+//                std::cout << "Permissions are " << status.permissions() << std::endl;
+//                std::time_t t = bf::last_write_time(v);
+//                auto last_modified = std::ctime(&t);
+//                std::cout << "Last write time is " << last_modified << std::endl;
+//                boost::system::error_code ec;
+//                boost::uintmax_t fsize = bf::file_size(v, ec);
+//                if (!ec) std::cout << "File size is " << fsize << std::endl;
+//                else std::cout << "File_size failed" << std::endl;
+            } else std::cout << "No file or directory found: " << v << std::endl;
         }
     }
     return 0;
